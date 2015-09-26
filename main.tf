d50ad03e3640687053950f5a5bdbb89690651391
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-26 19:05:27 +0200

    multi regeion update

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+/* Setup our aws provider */
+provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+  region      = "${var.region}"
+}
 resource "aws_vpc" "bdu" {
   cidr_block = "${var.cidr}"
   tags { Name = "${var.name}" }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,6 @@
 variable "name" { }
 variable "cidr" { }
+/* provider */
+variable "region"               {}
+variable "access_key"           {}
+variable "secret_key"           {}
